a2fcb02656179a0bb98d2a141899694a41325267
= <skripnik.new@gmail.com>
2022-01-25 18:09:41 +0300

    Prune unused images

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-     return 'Hello World from new CI/CD! and corporate nginx network!';
+     return 'Hello World from new CI/CD with local network!';
   }
 }
